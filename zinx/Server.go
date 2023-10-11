f696e9b864e7e062d9c6a1da7addb741c3323534
Lzz1260186691 <1260186691@qq.com>
2023-10-11 17:32:25 +0800

    10.11

--- zinx/Server.go
+++ zinx/Server.go
@@ -1,25 +1,49 @@
 package main
 
 import (
-	"TCP_Framework/zinx/ziface"
 	"TCP_Framework/zinx/znet"
 	"fmt"
+	"io"
+	"net"
 )
 
-type PingRouter struct {
-	znet.BaseRouter
-}
-
-func (this *PingRouter) Handle(request ziface.IRequest) {
-	fmt.Println("Call PingRouter Handle")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("ping...ping...ping\n"))
-	if err != nil {
-		fmt.Println("call back ping ping ping error")
-	}
-}
-
 func main() {
-	s := znet.NewServer()
-	s.AddRouter(&PingRouter{})
-	s.Serve()
+	listener, err := net.Listen("tcp", "127.0.0.1:7777")
+	if err != nil {
+		fmt.Println("server listen err: ", err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("server accept err:", err)
+		}
+		go func(conn net.Conn) {
+			dp := znet.NewDataPack()
+			for {
+				headData := make([]byte, dp.GetHeadLen())
+				_, err := io.ReadFull(conn, headData)
+				if err != nil {
+					fmt.Println("read head error")
+					break
+				}
+				msgHead, err := dp.Unpack(headData)
+				if err != nil {
+					fmt.Println("server unpack err:", err)
+					return
+				}
+				if msgHead.GetDataLen() > 0 {
+					msg := msgHead.(*znet.Message)
+					msg.Data = make([]byte, msg.GetDataLen())
+
+					_, err := io.ReadFull(conn, msg.Data)
+					if err != nil {
+						fmt.Println("server unpack data err: ", err)
+						return
+					}
+					fmt.Println("==> Recv Msg: ID=", msg.Id, ",len=", msg.DataLen, ",data=", string(msg.Data))
+				}
+
+			}
+		}(conn)
+	}
 }
---
+++ zinx/znet/datapack.go
@@ -0,0 +1,48 @@
+package znet
+
+import (
+	"TCP_Framework/zinx/utils"
+	"TCP_Framework/zinx/ziface"
+	"bytes"
+	"encoding/binary"
+	"errors"
+)
+
+type DataPack struct{}
+
+func NewDataPack() *DataPack {
+	return &DataPack{}
+}
+
+func (dp *DataPack) GetHeadLen() uint32 {
+	return 8
+}
+
+func (dp *DataPack) Pack(msg ziface.IMessage) ([]byte, error) {
+	dataBuff := bytes.NewBuffer([]byte{})
+	if err := binary.Write(dataBuff, binary.LittleEndian, msg.GetDataLen()); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(dataBuff, binary.LittleEndian, msg.GetMsgId()); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(dataBuff, binary.LittleEndian, msg.GetData()); err != nil {
+		return nil, err
+	}
+	return dataBuff.Bytes(), nil
+}
+
+func (dp *DataPack) Unpack(binaryData []byte) (ziface.IMessage, error) {
+	dataBuff := bytes.NewReader(binaryData)
+	msg := &Message{}
+	if err := binary.Read(dataBuff, binary.LittleEndian, &msg.DataLen); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(dataBuff, binary.LittleEndian, &msg.Id); err != nil {
+		return nil, err
+	}
+	if utils.GlobalObject.MaxpacketSize > 0 && msg.DataLen > utils.GlobalObject.MaxpacketSize {
+		return nil, errors.New("Too large msg data recieved")
+	}
+	return msg, nil
+}
